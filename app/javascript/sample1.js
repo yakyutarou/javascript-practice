b3656b983a332d8b52a09dcecaa97b911878bea8
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-04-30 12:27:22 +0900

    数値の操作

--- app/javascript/sample1.js
+++ app/javascript/sample1.js
@@ -1,7 +1,7 @@
 //アラートの表示
 alert("アラートを表示")
 
-//文字列の表示
+//文字列の操作
 let my = "カレー"
 console.log(my)
 
@@ -9,4 +9,14 @@ const word1 = "カレー"
 const word2 = "ラーメン"
 console.log(word1 + word2)
 console.log(word1 + "と" + word2)
+
+//数値の操作
+const string = "1"
+console.log(string)     //文字列はコンソールで黒字表示
+const integer = 2
+console.log(integer)    //数値コンソールで青字表示
+
+//数値の計算
+console.log(string+ string)   //コンソールで１１と表示
+console.log(integer+integer)  //2+2でコンソールで４と表示
 
