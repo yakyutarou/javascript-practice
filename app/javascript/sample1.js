77cc5ad6b7962a677d43eea3c548a506cf373abb
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-04-30 12:08:17 +0900

    文字列の操作

---
+++ app/javascript/sample1.js
@@ -0,0 +1,12 @@
+//アラートの表示
+alert("アラートを表示")
+
+//文字列の表示
+let my = "カレー"
+console.log(my)
+
+const word1 = "カレー"
+const word2 = "ラーメン"
+console.log(word1 + word2)
+console.log(word1 + "と" + word2)
+
