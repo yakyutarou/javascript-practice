1d0e91f0e004c0f6dd041f41751f2a4acdad00ad
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-05-01 13:41:56 +0900

    for文とforEach

--- app/javascript/sample1.js
+++ app/javascript/sample1.js
@@ -20,3 +20,30 @@ console.log(integer)    //数値コンソールで青字表示
 console.log(string+ string)   //コンソールで１１と表示
 console.log(integer+integer)  //2+2でコンソールで４と表示
 
+//for文
+for (let i=0; i<10; i++) {
+  console.log(i)
+}
+
+//for文と配列
+const myFruits = ["リンゴ","みかん","すいか"]
+let text = "私の好きな果物は"
+for (let i=0; i<myFruits.length; i++){
+  text += myFruits[i]
+  if(i !==myFruits.length-1){
+    text += "と"
+  }
+}
+text += "です。"
+console.log(text)
+
+//forEachと配列（出力結果は上と同じ）
+
+ myFruits.forEach(function(myFruit,i){
+   text += myFruit
+   if(i !== myFruits.length-1){
+     text += "と"
+   }
+ })
+ text += "です。"
+ console.log(text)
